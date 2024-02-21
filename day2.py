dc876b5b4872a0c3ae2d2f182db06580f7600c69
N SUBHAM RAO <111526347+subham-rao@users.noreply.github.com>
2024-02-21 11:19:44 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,24 @@
+# num = int(input("enter the number : "))
+# count = 0
+# if num % 2 == 0:
+#     print(num, "is even")
+# else:
+#     print(num, "is not even")
+# for i in range (1,num+1):
+#     if (num%i==0):
+#         count+=1
+#
+# if(num%i==0) and (count==2):
+#          print(num , "is prime")
+# else :
+#     print(num , "is not prime")
+
+
+# def getData(weather):
+#     return input(f"is it {weather}: ").lower()
+# def checking(data):
+#     while not (data == "yes" or data == "no"):
+#         return data
+# rain = checking(getData("Raining"))
+# snow = checking(getData("Snowing"))
+
